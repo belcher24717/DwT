ef3375af38102aecb5f585d2a0aeb6710e143513
belcher24717 <belcher24717@hotmail.com>
2018-04-22 20:31:59 -0700

    f

---
+++ Assets/navmeshtestscript.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class navmeshtestscript : MonoBehaviour
+{
+    public NavMeshAgent Agent;
+    public GameObject Destination;
+
+    // Use this for initialization
+    void Start()
+    {
+        //Agent = this.gameObject.GetComponent<NavMeshAgent>();
+        Agent.destination = Destination.transform.position;
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+}
