869bf8183b99487e30a580e939bff81437cd40c5
ryanyoung1989 <ryanyoung1989@hotmail.com>
2018-04-24 23:10:36 -0700

    add plane w/ navmesh

---
+++ Assets/NavMesh.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class NavMesh : MonoBehaviour
+{
+
+    public NavMeshAgent agent;
+    public GameObject Destination;
+
+	// Use this for initialization
+	void Start ()
+    {
+        agent.destination = Destination.transform.position;
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+		
+	}
+}
