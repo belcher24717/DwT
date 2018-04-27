c22211cac55ef1cacbcadfed0d6a5dae2cdf1872
ryanyoung1989 <ryanyoung1989@hotmail.com>
2018-04-26 20:53:02 -0700

    Camera Movement and WIP Tower Spawn
    -Camera now moves with WASD and Arrow Keys (eventually will be w/ mouse moving to edge of screen too)
    -Added script to spawn towers on left click, not working. Currently attached to Camera...

---
+++ Assets/UI/ClickSpawnScript.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ClickSpawnScript : MonoBehaviour
+{
+
+    public GameObject tower;        // The tower prefab to place on click (will be passed in, will need checks for resources, etc)
+    public LayerMask mask;          // Not sure what this is atm?
+
+    private Vector3 buildPoint;     // the point to build the tower
+    private RaycastHit hit;         // The Ray from camera to point-of-click 
+
+    // Use this for initialization
+    void Start ()
+    {
+        // eventually this will be removed. A tower will be selected and passed in...
+        //TODO: REMOVE THIS
+        tower = GameObject.Find("Tower");
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+		if (Input.GetMouseButton(0))
+        {
+            Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+            if (Physics.Raycast(ray, out hit, mask))
+            {
+                // only spawn if the floor is clicked
+                if (hit.transform.name == "Floor")
+                {
+                    buildPoint = hit.point;
+                    Instantiate(tower, buildPoint, Quaternion.identity);
+                }
+            }
+        }
+	}
+}
---
+++ Assets/UI/MoveCameraScript.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+public class MoveCameraScript : MonoBehaviour
+{
+
+    // Use this for initialization
+    void Start ()
+    {
+
+    }
+	
+	// Update is called once per frame
+	void Update ()
+    {
+        // probably want this to also incorporate edge of screen mouse movement, also, should not double speed when common-direction keys are pressed...
+        if (Input.GetKey(KeyCode.A) || Input.GetKey(KeyCode.LeftArrow))
+        {
+            transform.Translate(new Vector3(-15f * Time.deltaTime, 0, 0));
+        }
+
+        if (Input.GetKey(KeyCode.D) || Input.GetKey(KeyCode.RightArrow))
+        {
+            transform.Translate(new Vector3(15f * Time.deltaTime, 0, 0));
+        }
+
+        if (Input.GetKey(KeyCode.W) || Input.GetKey(KeyCode.UpArrow))
+        {
+            transform.Translate(new Vector3(0, 15f * Time.deltaTime, 0));
+        }
+
+        if (Input.GetKey(KeyCode.S) || Input.GetKey(KeyCode.DownArrow))
+        {
+            transform.Translate(new Vector3(0, -15F * Time.deltaTime, 0));
+        }
+
+    }
+}
