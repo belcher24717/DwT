f0fe928ed4446f85da8ffb7b90fce24aa74627e4
ryanyoung1989 <ryanyoung1989@hotmail.com>
2018-04-27 23:23:08 -0700

    fixed spawn script

--- Assets/UI/ClickSpawnScript.cs
+++ Assets/UI/ClickSpawnScript.cs
@@ -6,7 +6,6 @@ public class ClickSpawnScript : MonoBehaviour
 {
 
     public GameObject tower;        // The tower prefab to place on click (will be passed in, will need checks for resources, etc)
-    public LayerMask mask;          // Not sure what this is atm?
 
     private Vector3 buildPoint;     // the point to build the tower
     private RaycastHit hit;         // The Ray from camera to point-of-click 
@@ -14,9 +13,7 @@ public class ClickSpawnScript : MonoBehaviour
     // Use this for initialization
     void Start ()
     {
-        // eventually this will be removed. A tower will be selected and passed in...
-        //TODO: REMOVE THIS
-        tower = GameObject.Find("Tower");
+
 	}
 	
 	// Update is called once per frame
@@ -25,7 +22,7 @@ public class ClickSpawnScript : MonoBehaviour
 		if (Input.GetMouseButton(0))
         {
             Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
-            if (Physics.Raycast(ray, out hit, mask))
+            if (Physics.Raycast(ray, out hit))
             {
                 // only spawn if the floor is clicked
                 if (hit.transform.name == "Floor")
