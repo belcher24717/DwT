1b51d23614c8813b21b450bde5dc40cbafeaf39b
ryanyoung1989 <ryanyoung1989@hotmail.com>
2018-04-28 21:46:44 -0700

    Added prelim grid
    Now towers only build on the grid, can't be placed on top of each other

---
+++ Assets/Classes/Grid.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Grid : MonoBehaviour {
+
+    [SerializeField]
+    private float size = 1f;
+
+    public Vector3 GetNearestPointOnGrid (Vector3 buildPosition)
+    {
+        int xCount = Mathf.RoundToInt(buildPosition.x / size);
+        int yCount = Mathf.RoundToInt(buildPosition.y / size);
+        int zCount = Mathf.RoundToInt(buildPosition.z / size);
+
+        return new Vector3((float)xCount, (float)yCount, (float)zCount);
+    }
+
+}
--- Assets/UI/ClickSpawnScript.cs
+++ Assets/UI/ClickSpawnScript.cs
@@ -5,10 +5,18 @@ using UnityEngine;
 public class ClickSpawnScript : MonoBehaviour
 {
 
-    public GameObject tower;        // The tower prefab to place on click (will be passed in, will need checks for resources, etc)
+    public GameObject tower;                            // The tower prefab to place on click (will be passed in, will need checks for resources, etc)
 
-    private Vector3 buildPoint;     // the point to build the tower
-    private RaycastHit hit;         // The Ray from camera to point-of-click 
+    private Vector3 buildPoint;                         // the point to build the tower
+    private RaycastHit hit;                             // The Ray from camera to point-of-click 
+    private Grid grid;                                  // The grid for placing towers
+
+    private ArrayList placedTowers = new ArrayList();   // A list of all placed towers
+
+    void Awake ()
+    {
+        grid = FindObjectOfType<Grid>();
+    }
 
     // Use this for initialization
     void Start ()
@@ -27,10 +35,25 @@ public class ClickSpawnScript : MonoBehaviour
                 // only spawn if the floor is clicked
                 if (hit.transform.name == "Floor")
                 {
-                    buildPoint = hit.point;
-                    Instantiate(tower, buildPoint, Quaternion.identity);
+                    PlaceTowerOnGrid(hit.point);
                 }
             }
         }
 	}
+
+    void PlaceTowerOnGrid (Vector3 buildPoint)
+    {
+        Vector3 gridPosition = grid.GetNearestPointOnGrid(buildPoint);
+
+        // if a tower already exists in this location, don't place it
+        foreach (GameObject tower in placedTowers)
+        {
+            if (gridPosition == tower.transform.position)
+            {
+                return;
+            }
+        }
+        // place the tower
+        placedTowers.Add(Instantiate(tower, gridPosition, Quaternion.identity));
+    }
 }
