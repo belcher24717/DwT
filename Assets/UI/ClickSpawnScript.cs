8274656ca520bfb26a67c8122fd3e2e9d47bf4f0
belcher24717 <belcher24717@hotmail.com>
2018-05-05 20:43:30 -0700

    added selected tower on cursor
    - selecting a tower causes that tower to show on cursor for where it will place

--- Assets/UI/ClickSpawnScript.cs
+++ Assets/UI/ClickSpawnScript.cs
@@ -5,14 +5,43 @@ using UnityEngine;
 
 public class ClickSpawnScript : MonoBehaviour
 {
-    private Vector3 buildPoint;                         // the point to build the tower
     private RaycastHit hit;                             // The Ray from camera to point-of-click
     [SerializeField]
     private Grid grid;                                  // The grid for placing towers
-
+    private static GameObject cursorTower;
+    private GameObject placedCursorTower;
+    protected bool gridPositionChanged;
     private ArrayList placedTowers = new ArrayList();   // A list of all placed towers
 
     private static List<SelectTowerScript> _towers;
+
+    private static GameObject selectedTower;
+    public static GameObject SelectedTower
+    {
+        get
+        {
+            if (selectedTower == null)
+                cursorTower = selectedTower = _towers.FirstOrDefault(t => t.Selected)?.Tower;
+            return selectedTower;
+        }
+    }
+
+    private Vector3 gridPosition;
+    public Vector3 GridPosition
+    {
+        get
+        {
+            return gridPosition;
+        }
+        private set
+        {
+            if (gridPosition != value)
+            {
+                gridPositionChanged = true;
+                gridPosition = value;
+            }
+        }
+    }
 
     // Use this for initialization
     void Start()
@@ -23,43 +52,66 @@ public class ClickSpawnScript : MonoBehaviour
     // Update is called once per frame
     void Update ()
     {
-		if (Input.GetMouseButton(0))
+        Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+        bool rayHit = Physics.Raycast(ray, out hit);
+
+        // only place towers on floor
+        if (rayHit && hit.transform.name == "Floor")
         {
-            Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
-            if (Physics.Raycast(ray, out hit))
+            GridPosition = grid.GetNearestPointOnGrid(hit.point);
+
+            // if girdPosition changed, place new cursorTower and cleanup old
+            if (gridPositionChanged && SelectedTower != null)
             {
-                // only spawn if the floor is clicked
-                if (hit.transform.name == "Floor")
-                {
-                    PlaceTowerOnGrid(hit.point);
-                }
+                gridPositionChanged = false;
+                PlaceTowerOnGrid(hit.point, true);
             }
+
+            // if left mouse click, attempt to place tower
+            if (Input.GetMouseButton(0))
+                 PlaceTowerOnGrid(hit.point, false);
+        }
+        else
+        {
+            if (placedCursorTower != null)
+                GameObject.Destroy(placedCursorTower);
         }
 	}
 
     protected void ResetSelection()
     {
         _towers.ForEach(t => t.Selected = false);
+        selectedTower = null;
     }
 
-    void PlaceTowerOnGrid (Vector3 buildPoint)
+    void PlaceTowerOnGrid (Vector3 buildPoint, bool placeCursorTower)
     {
-        GameObject selectedTower = _towers.FirstOrDefault(t => t.Selected)?.Tower;
-        if (selectedTower == null)
+
+        GameObject towerToUse = placeCursorTower ? cursorTower : SelectedTower;
+        if (towerToUse == null)
             return;
 
-        Vector3 gridPosition = grid.GetNearestPointOnGrid(buildPoint);
-
         // if a tower already exists in this location, don't place it
-        foreach (GameObject tower in placedTowers)
+        if (!placeCursorTower)
         {
-            if (gridPosition == tower.transform.position)
+            foreach (GameObject tower in placedTowers)
             {
-                return;
+                if (GridPosition == tower.transform.position)
+                {
+                    return;
+                }
             }
         }
 
+        // delete old placed cursor tower
+        if (placedCursorTower != null)
+            GameObject.Destroy(placedCursorTower);
+
         // place the tower
-        placedTowers.Add(Instantiate(selectedTower, gridPosition, Quaternion.identity));
+        GameObject placedTower = Instantiate(towerToUse, GridPosition, Quaternion.identity);
+        if (!placeCursorTower)
+            placedTowers.Add(placedTower);
+        else
+            placedCursorTower = placedTower; 
     }
 }
